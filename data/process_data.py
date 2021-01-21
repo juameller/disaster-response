15d8ad571024f28be73e1b1ab94aaee919800feb
jc-cmelgarejo <jcmelgarejo3@gmail.com>
2021-01-21 17:51:38 +0100

    feat: Parse user arguments

--- data/process_data.py
+++ data/process_data.py
@@ -1,5 +1,5 @@
 import sys
-
+import argparse
 
 def load_data(messages_filepath, categories_filepath):
     pass
@@ -12,32 +12,35 @@ def clean_data(df):
 def save_data(df, database_filename):
     pass  
 
+def parse_inputs():
+    parser = argparse.ArgumentParser(description='ETL Pipeline')
+    parser.add_argument('messages_filepath', type = str, help = 'Message dataset')
+    parser.add_argument('category_filepath', type = str, help = 'Categories dataset')
+    parser.add_argument('database_filepath', type = str, help = 'Database')
+    return parser.parse_args()
 
 def main():
-    if len(sys.argv) == 4:
+    print("--Exeuting ETL Pipeline--")
+    args = parse_inputs()
+   
+    messages_filepath, categories_filepath, database_filepath = args.messages_filepath, \
+                                                                args.category_filepath, \
+                                                                args.database_filepath
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+    print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'.format(messages_filepath, 
+                                                                         categories_filepath))
+    df = load_data(messages_filepath, categories_filepath)
 
-        print('Cleaning data...')
-        df = clean_data(df)
+    print('Cleaning data...')
+    df = clean_data(df)
         
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
+    print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+    save_data(df, database_filepath)
         
-        print('Cleaned data saved to database!')
+    print('Cleaned data saved to database!')
     
-    else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
-
+    
 
 if __name__ == '__main__':
     main()
